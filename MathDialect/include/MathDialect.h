aecbb107d71c770c647a79b9d8e4cfab3087ef2a
MarkVeerasingam <markveer70@gmail.com>
2026-01-19 14:40:32 +0000

    registered core dialect

---
+++ MathDialect/include/MathDialect.h
@@ -0,0 +1,9 @@
+#ifndef MATH_MATHDIALECT_H
+#define MATH_MATHDIALECT_H
+
+#include "mlir/Bytecode/BytecodeOpInterface.h"
+#include "mlir/IR/Dialect.h"
+
+#include "MathDialect.h.inc"
+
+#endif // MATH_MATHDIALECT_H
---
+++ MathDialect/lib/MathDialect.cpp
@@ -0,0 +1,17 @@
+#include "MathDialect.h"
+
+using namespace mlir;
+using namespace mlir::math;
+
+#include "MathDialect.cpp.inc"
+
+//===----------------------------------------------------------------------===//
+// Math dialect.
+//===----------------------------------------------------------------------===//
+
+void MathDialect::initialize() {
+  addOperations<
+#define GET_OP_LIST
+      >();
+  registerTypes();
+}
