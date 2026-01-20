b892a8b0d6f9c099d32a64bdd33371513a93f651
MarkVeerasingam <markveer70@gmail.com>
2026-01-20 22:48:08 +0000

    added math-opt driver & updated cmake naming conventions for the dialect

---
+++ MathDialect/math-opt/math-opt.cpp
@@ -0,0 +1,23 @@
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/IR/DialectRegistry.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/Tools/mlir-opt/MlirOptMain.h"
+
+#include "MathDialect.h"
+
+int main(int argc, char **argv)
+{
+    mlir::DialectRegistry registry;
+
+    // Only register the dialects we are actually linking in CMake
+    registry.insert<mlir::math::MathDialect,
+                    mlir::arith::ArithDialect,
+                    mlir::func::FuncDialect>();
+
+    // Note: mlir::registerAllPasses() is removed to avoid undefined references
+    // If you need specific passes (like -canonicalize), register them individually.
+
+    return mlir::asMainReturnCode(
+        mlir::MlirOptMain(argc, argv, "Math optimizer driver\n", registry));
+}
---
+++ MathDialect/tests/test_constant.mlir
@@ -0,0 +1,4 @@
+func.func @main() {
+  %0 = math.constant 10 : i32
+  return
+}
